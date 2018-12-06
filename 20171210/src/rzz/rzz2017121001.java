1484bf6b5f4fc07edd0ec1d0881f5af3044e774e
RZZBlackMagic <1403741992@qq.com>
2018-12-06 22:25:58 +0800

    test

---
+++ 20171210/src/rzz/rzz2017121001.java
@@ -0,0 +1,86 @@
+package rzz;
+import java.util.Arrays;
+public class rzz2017121001 {
+	String s="One World One Dream";
+	public static void main(String[] args) {
+		/*Scanner scan=new Scanner(System.in);
+		int i,j,s;
+		for(i=1;i<10;i++){
+			for(j=1;j<=i;j++){
+				s=i*j;
+				System.out.print(s+"="+i+"*"+j+"\t");
+			}
+			System.out.println();
+		}
+		
+		int i,j,flag;
+	    int []arr=new int[10];
+	    for(i=0;i<arr.length;i++){
+	    	arr[i]=(int)(Math.random()*100);
+	    	
+	    }
+	    for(i=0;i<arr.length;i++){
+	    	System.out.print(arr[i]+" ");
+	    }
+	    System.out.println();
+	    int []Arr=Arrays.copyOf(arr, arr.length);
+	    for(i=0;i<arr.length-1;i++){
+	    	if(arr[i]>arr[i+1]){
+	    		flag=arr[i];
+	    		arr[i]=arr[i+1];
+	    		arr[i+1]=flag;
+	    	}
+	    }
+	    for(i=0;i<arr.length;i++){
+	    	
+	    
+	          System.out.print(arr[i]+" ");
+	    }
+	    System.out.println();
+	    
+	     Arr = Arrays.copyOf(Arr,Arr.length+1);
+	     //int s=Arr.length;
+	     //int m=arr.length;
+	     //System.out.println(s+" "+m);
+	    Arr[Arr.length-1]=arr[arr.length-1];
+	    for(i=0;i<Arr.length;i++){
+	    	System.out.print(Arr[i]+" ");
+	    }
+
+		int []arr=new int[10];
+	
+		for(int i=0;i<10;i++){
+			arr[i]=(int)(Math.random()*100);
+		}
+		for(int i=0;i<10;i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		int s=arr.length;
+		System.out.println(s);
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j]>arr[j+1]){
+					int flag=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=flag;
+							
+				}
+			}
+			
+			
+			
+		}
+		for(int i=0;i<10;i++){
+		    System.out.print(arr[i]+" ");
+		}
+		
+		Arrays.sort(arr);
+		for(i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.println(s);*/
+	}
+
+}

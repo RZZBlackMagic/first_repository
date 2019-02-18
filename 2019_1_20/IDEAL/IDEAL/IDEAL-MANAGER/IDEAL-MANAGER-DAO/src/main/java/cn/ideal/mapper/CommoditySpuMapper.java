6ef577970561f74e407104bcc3284e4b4aa69887
RZZBlackMagic <1403741992@qq.com>
2019-02-18 22:27:35 +0800

    ideal

---
+++ 2019_1_20/IDEAL/IDEAL/IDEAL-MANAGER/IDEAL-MANAGER-DAO/src/main/java/cn/ideal/mapper/CommoditySpuMapper.java
@@ -0,0 +1,31 @@
+package cn.ideal.mapper;
+
+import cn.ideal.pojo.CommoditySpu;
+import cn.ideal.pojo.CommoditySpuExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CommoditySpuMapper {
+    int countByExample(CommoditySpuExample example);
+
+    int deleteByExample(CommoditySpuExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CommoditySpu record);
+
+    int insertSelective(CommoditySpu record);
+
+    List<CommoditySpu> selectByExample(CommoditySpuExample example);
+
+    CommoditySpu selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") CommoditySpu record, @Param("example") CommoditySpuExample example);
+
+    int updateByExample(@Param("record") CommoditySpu record, @Param("example") CommoditySpuExample example);
+
+    int updateByPrimaryKeySelective(CommoditySpu record);
+
+    int updateByPrimaryKey(CommoditySpu record);
+}
---
+++ 2019_1_20/IDEAL/IDEAL/_MyBatis/src/main/java/cn/ideal/pojo/CommoditySpu.java
@@ -0,0 +1,73 @@
+package cn.ideal.pojo;
+
+public class CommoditySpu {
+    private Long id;
+
+    private String title;
+
+    private Long cid;
+
+    private Long apid;
+
+    private String sellpoint;
+
+    private String image;
+
+    private Byte status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Long getCid() {
+        return cid;
+    }
+
+    public void setCid(Long cid) {
+        this.cid = cid;
+    }
+
+    public Long getApid() {
+        return apid;
+    }
+
+    public void setApid(Long apid) {
+        this.apid = apid;
+    }
+
+    public String getSellpoint() {
+        return sellpoint;
+    }
+
+    public void setSellpoint(String sellpoint) {
+        this.sellpoint = sellpoint == null ? null : sellpoint.trim();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image == null ? null : image.trim();
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}

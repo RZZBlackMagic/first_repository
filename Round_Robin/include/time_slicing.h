f390a87ce88e0cb72d3f8504ece81160c4d2bec7
RZZBlackMagic <1403741992@qq.com>
2020-04-06 22:09:52 +0800

    round robin

---
+++ Round_Robin/include/time_slicing.h
@@ -0,0 +1,16 @@
+#ifndef TIME_SLICING_H
+#define TIME_SLICING_H
+
+
+class time_slicing
+{
+    public:
+        time_slicing();
+        virtual ~time_slicing();
+
+    protected:
+
+    private:
+};
+
+#endif // TIME_SLICING_H
---
+++ Round_Robin/src/time_slicing.cpp
@@ -0,0 +1,11 @@
+#include "time_slicing.h"
+
+time_slicing::time_slicing()
+{
+    //ctor
+}
+
+time_slicing::~time_slicing()
+{
+    //dtor
+}
